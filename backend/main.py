586bfd88332007f8f3d15eca86cffb4d18611762
Florian Fellner <ffellner75@gmail.com>
2026-01-06 16:10:17 +0100

    use bart for evaluation

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,25 @@
 from fastapi import FastAPI
 from typing import List
-import numpy as np
-import torch
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModel, LlamaTokenizer, LlamaForCausalLM
+from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassification
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
-import os
-import pickle
-import torch.nn.functional as F
+from time import time
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 from sentence_transformers import CrossEncoder
-import json
+import numpy as np
+import torch
+import os
+import pickle
+import torch.nn.functional as F
 
 models = {"sbert": "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", "mbert": "bert-base-multilingual-cased", "tf-idf": "tf-idf"}
 MODEL_NAME = models["sbert"]
 MODEL_RERANKER = "cross-encoder/mmarco-mMiniLMv2-L12-H384-v1"
-MODEL_EVAL  = 'openlm-research/open_llama_3b_v2'
-
+MODEL_EVAL  = "facebook/bart-large-mnli"
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 MAX_LENGTH = 256
@@ -29,8 +27,8 @@ TOP_K = 5
 
 tokenizer = None
 model = None
-gpt_model = None
-gpt_tokenizer = None
+eval_model = None
+eval_tokenizer = None
 reranker = None
 
 tfidf_vectorizer = None
@@ -200,10 +198,10 @@ def switch_model(new_model_name, subset_size=2000):
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
-        global reranker, gpt_model, gpt_tokenizer
+        global reranker, eval_model, eval_tokenizer
         reranker = CrossEncoder(MODEL_RERANKER, device=DEVICE)
-        gpt_tokenizer = LlamaTokenizer.from_pretrained(MODEL_EVAL)
-        gpt_model = LlamaForCausalLM.from_pretrained(MODEL_EVAL, dtype=torch.float16, use_safetensors=True).to(DEVICE)
+        eval_tokenizer = AutoTokenizer.from_pretrained(MODEL_EVAL)
+        eval_model = AutoModelForSequenceClassification.from_pretrained(MODEL_EVAL)
         startup(subset_size=2000)
         print("Startup completed successfully")
         yield
@@ -239,9 +237,8 @@ def answer_query(query, top_k, rerank):
                 "url": para_urls[idx],
                 "score": float(ce_score),
                 "paragraph": paragraphs[idx],
+                "eval": eval(query, paragraphs[idx])
             })
-
-        gpt_eval(query, results[0]['paragraph'])
         
         return results
 
@@ -255,43 +252,33 @@ def answer_query(query, top_k, rerank):
             "url": para_urls[idx],
             "score": float(scores[idx]),
             "paragraph": paragraphs[idx],
+            "eval": eval(query, paragraphs[idx])
         })
 
     return results
 
 ### Evalutaion ###
 
-def gpt_eval(query, paragraph):
-    from time import time
-    
-    start = time.time()
-    
-    prompt = f"""
-            You are evaluating factual support.
+def eval(query, paragraph):
+    start = time()
 
-            Claim:
-            "{query}"
-
-            Evidence:
-            "{paragraph}"
-
-            Question:
-            Does the evidence SUPPORT the claim, CONTRADICT it, or is it NOT MENTIONED?
-
-            Answer with exactly one of:
-            SUPPORTS
-            CONTRADICTS
-            NOT_MENTIONED
-            """.strip()
-    
-    input_ids = gpt_tokenizer(prompt, return_tensors="pt").input_ids
-
-    generation_output = gpt_model.generate(
-        input_ids=input_ids, max_new_tokens=32
+    inputs = eval_tokenizer(
+        paragraph,
+        query,
+        return_tensors="pt",
+        truncation=True
     )
-    print(gpt_tokenizer.decode(generation_output[0]))
 
-    print(f"GPT Eval took {time.time() - start:.2f} seconds")
+    logits = eval_model(**inputs).logits
+    prediction = logits.argmax(dim=1).item()
+
+    label_map = {
+        0: "CONTRADICTS",
+        1: "NOT MENTIONED",
+        2: "SUPPORTS"
+    }
+
+    return label_map[prediction]
     
     
 ### FastAPI App ###
@@ -308,7 +295,7 @@ app.add_middleware(
 
 class RequestBody(BaseModel):
     method: str
-    ce: bool = False
+    ce: bool = True
     query: str
 
 @app.post("/check")
